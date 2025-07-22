23ea7b49798aae1d6eeb771db153dbced3aeda15
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-07-22 17:01:56 +0900

    Update model.py

--- flux/model.py
+++ flux/model.py
@@ -155,14 +155,14 @@ class Flux_kv(Flux):
         cnt = 0
         for block in self.double_blocks:
             info['id'] = cnt
-            img, txt = block(img=img, txt=txt, vec=vec, pe=pe, info=info, info_s=info_s, zt_r)
+            img, txt = block(img=img, txt=txt, vec=vec, pe=pe, info=info, info_s=info_s, zt_r=zt_r)
             cnt += 1
 
         cnt = 0
         x = torch.cat((txt, img), 1) 
         for block in self.single_blocks:
             info['id'] = cnt
-            x = block(x, vec=vec, pe=pe, info=info, info_s=info_s, zt_r)
+            x = block(x, vec=vec, pe=pe, info=info, info_s=info_s, zt_r=zt_r)
             cnt += 1
 
         img = x[:, txt.shape[1] :, ...]
