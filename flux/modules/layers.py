a9bcc745ffaa6465f34e16ef0214f82510a4bbef
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-08-05 17:00:03 +0900

    Update layers.py

--- flux/modules/layers.py
+++ flux/modules/layers.py
@@ -429,7 +429,8 @@ class DoubleStreamBlock_kv(DoubleStreamBlock):
                 tokenizer=info['tokenizer'],         # info에 tokenizer 추가되어야 함
                 prompts="a dog is lying on the floor",           # info에 caption도 있어야 함
                 base_dir='attn_overlay',
-                unconditional=False
+                unconditional=False,
+                hw=(48, 32)
             )
 
             
