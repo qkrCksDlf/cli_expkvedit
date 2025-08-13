3212347655989b64c610994d0ff4a7120572cf8e
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-08-14 01:59:26 +0900

    Update sampling.py

--- flux/sampling.py
+++ flux/sampling.py
@@ -156,29 +156,30 @@ def denoise_kv(
         timesteps = timesteps[::-1]
         
     guidance_vec = torch.full((img.shape[0],), guidance, device=img.device, dtype=img.dtype)
+
+    if not inverse:
+        img_name = str(info['t']) + '_' + 'img'
+        mask_indices = info['mask_indices']
+        zt_r= zt_r[:, mask_indices,...]
+        tar_img = info['feature'][img_name].to(img.device)
+        zt_r = tar_img[:, info['mask_indices'],...] * (1 - info['mask'][:, info['mask_indices'],...]) + zt_r * info['mask'][:, info['mask_indices'],...]
+        
+        
     
     for i, (t_curr, t_prev) in enumerate(tzip(timesteps[:-1], timesteps[1:])):
         t_vec = torch.full((img.shape[0],), t_curr, dtype=img.dtype, device=img.device)
         info['t'] = t_prev if inverse else t_curr
+        
         
         if inverse:
             img_name = str(info['t']) + '_' + 'img'
             info['feature'][img_name] = img.cpu()
         else:
             mask_indices = info['mask_indices']
-            print(img.shape)
-            zt_r2= zt_r[:, mask_indices,...]
-            print(zt_r2.shape)
-            input()
             img_name = str(info['t']) + '_' + 'img'
             source_img = info_s['feature'][img_name].to(img.device)
             img = source_img[:, info['mask_indices'],...] * (1 - info['mask'][:, info['mask_indices'],...]) + img * info['mask'][:, info['mask_indices'],...]
             
-            
-            
-            zt_r = source_img[:, info['mask_indices'],...] * (1 - info['mask'][:, info['mask_indices'],...]) + zt_r['mask_indices'] * info['mask'][:, info['mask_indices'],...]
-            
-            print("이거 맞는데")
         pred = model(
             img=img,
             img_ids=img_ids,
