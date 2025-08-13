bb63a224d37933ad44dead9028c4d71785f7b51b
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-08-14 00:50:02 +0900

    Update layers.py

--- flux/modules/layers.py
+++ flux/modules/layers.py
@@ -385,14 +385,14 @@ class DoubleStreamBlock_kv(DoubleStreamBlock):
             
             
             
-            # img_mod1, img_mod2 = self.img_mod(vec)
-            # txt_mod1, txt_mod2 = self.txt_mod(vec)
-            # # prepare reference image for attention
-            # img_modulated_r = self.img_norm1(zt_r)
-            # img_modulated_r = (1 + img_mod1.scale) * img_modulated_r + img_mod1.shift
-            # img_qkv_r = self.img_attn.qkv(img_modulated_r)
-            # img_q_r, img_k_r, img_v_r = rearrange(img_qkv_r, "B L (K H D) -> K B H L D", K=3, H=self.num_heads)          
-            # img_q_r, img_k_r = self.img_attn.norm(img_q_r, img_k_r, img_v_r)
+            img_mod1, img_mod2 = self.img_mod(vec)
+            txt_mod1, txt_mod2 = self.txt_mod(vec)
+            # prepare reference image for attention
+            img_modulated_r = self.img_norm1(zt_r)
+            img_modulated_r = (1 + img_mod1.scale) * img_modulated_r + img_mod1.shift
+            img_qkv_r = self.img_attn.qkv(img_modulated_r)
+            img_q_r, img_k_r, img_v_r = rearrange(img_qkv_r, "B L (K H D) -> K B H L D", K=3, H=self.num_heads)          
+            img_q_r, img_k_r = self.img_attn.norm(img_q_r, img_k_r, img_v_r)
             
             
             source_img_k = info['feature'][feature_k_name].to(img.device) #레퍼런스
@@ -406,8 +406,10 @@ class DoubleStreamBlock_kv(DoubleStreamBlock):
             mask_indices = info['mask_indices'] 
             #source_img_k[:, :, mask_indices, ...] = img_k
             #source_img_v[:, :, mask_indices, ...] = img_v
-            source_img_k_s[:, :, mask_indices, ...] = source_img_k[:, :, mask_indices, ...]
-            source_img_v_s[:, :, mask_indices, ...] = source_img_v[:, :, mask_indices, ...]
+            #source_img_k_s[:, :, mask_indices, ...] = source_img_k[:, :, mask_indices, ...]
+            #source_img_v_s[:, :, mask_indices, ...] = source_img_v[:, :, mask_indices, ...]
+            source_img_k_s[:, :, mask_indices, ...] = img_k_r
+            source_img_v_s[:, :, mask_indices, ...] = img_v_r
 
             
             
