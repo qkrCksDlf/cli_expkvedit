6c4cfcd82e3c6621e752bdfadcdbbb2bba68a865
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-08-19 10:57:19 +0900

    Update kv_edit.py

--- models/kv_edit.py
+++ models/kv_edit.py
@@ -211,7 +211,7 @@ class Flux_kv_edit(only_Flux):
             
         else:
             img_name = str(info['t']) + '_' + 'img'
-            zt_r = info_s['feature'][img_name].to(zt_r.device)
+            #zt_r = info_s['feature'][img_name].to(zt_r.device)
             inp_target["img"] = zt_r[:, mask_indices,...]
             
         if opts.attn_scale != 0 and (~bool_mask).any():
