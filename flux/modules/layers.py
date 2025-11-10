d711525b5a6457d52d1ae4eebc99a0279d32bfc5
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-11-10 09:46:00 +0900

    Update layers.py

--- flux/modules/layers.py
+++ flux/modules/layers.py
@@ -537,7 +537,7 @@ class SingleStreamBlock_kv(SingleStreamBlock):
 
             print(info['t'])
             
-            if info['t'] < 0.55:
+            if info['t'] > 0.55:
                 print("실행!")
                 source_img_k_s[:, :, mask_indices, ...] = source_img_k[:, :, mask_indices, ...]
                 source_img_v_s[:, :, mask_indices, ...] = source_img_v[:, :, mask_indices, ...]
