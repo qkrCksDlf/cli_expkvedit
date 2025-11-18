a97d161859aca97f3798c8eea427c208754b15f0
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-11-18 09:52:21 +0900

    Update layers.py

--- flux/modules/layers.py
+++ flux/modules/layers.py
@@ -679,14 +679,14 @@ class SingleStreamBlock_kv(SingleStreamBlock):
 
             print(info['t'])
             
-            if info['t'] < 0.745 and 15 < info['id'] < 31:
+            if info['t'] < 0.773 and info['id']==16:
                 print("실행!")
                 info['mask'] = info['union_mask']
                 info['mask_indices'] = info['union_mask_indices']
-                #source_img_k_s[:, :, mask_indices, ...] = source_img_k[:, :, mask_indices, ...]
-                #source_img_v_s[:, :, mask_indices, ...] = source_img_v[:, :, mask_indices, ...]
-                source_img_k_s[:, :, mask_indices, ...] = img_k
-                source_img_v_s[:, :, mask_indices, ...] = img_v
+                source_img_k_s[:, :, mask_indices, ...] = source_img_k[:, :, mask_indices, ...]
+                source_img_v_s[:, :, mask_indices, ...] = source_img_v[:, :, mask_indices, ...]
+                # source_img_k_s[:, :, mask_indices, ...] = img_k
+                # source_img_v_s[:, :, mask_indices, ...] = img_v
             else:
                 source_img_k_s[:, :, mask_indices, ...] = img_k
                 source_img_v_s[:, :, mask_indices, ...] = img_v
