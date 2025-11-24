7056678cb5b8cc6ecf5202ebebf4ad7b0381bbfc
choboja <95958789+qkrCksDlf@users.noreply.github.com>
2025-11-25 07:28:33 +0900

    Update layers.py

--- flux/modules/layers.py
+++ flux/modules/layers.py
@@ -679,7 +679,7 @@ class SingleStreamBlock_kv(SingleStreamBlock):
 
             print(info['t'])
             
-            if info['t'] < 0.34:
+            if info['t'] >100:
                 print("실행!")
                 #info['mask'] = info['union_mask']
                 #info['mask_indices'] = info['union_mask_indices']
